9f2aab45f35a9ecd766f03ef71ba30f258361a3b
GabrielFdez6 <zS23014071@estudiantes.uv.mx>
2026-01-11 21:10:31 -0600

    Corrige error visual del chat de texto cuando se desconecta el internet y agrega scroll automatico cuando aparece un nuevo mensaje

--- ConquiánCliente/View/Lobby/LobbyGame.xaml.cs
+++ ConquiánCliente/View/Lobby/LobbyGame.xaml.cs
@@ -1,12 +1,10 @@
-﻿using System.Windows;
-using ConquiánCliente.ViewModel;
+﻿using ConquiánCliente.ViewModel;
 using ConquiánCliente.ViewModel.Lobby;
+using System.Collections.Specialized;
+using System.Windows;
 
 namespace ConquiánCliente.View.Lobby
 {
-    /// <summary>
-    /// Lógica de interacción para LobbyGame.xaml
-    /// </summary>
     public partial class LobbyGame : Window
     {
         public LobbyGame(string roomCode)
@@ -14,6 +12,7 @@ namespace ConquiánCliente.View.Lobby
             InitializeComponent();
             DataContext = new LobbyGameViewModel(roomCode);
             this.Closing += LobbyGame_Closing;
+            this.Loaded += LobbyGame_Loaded;
         }
 
         private void LobbyGame_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -29,6 +28,24 @@ namespace ConquiánCliente.View.Lobby
                 if (vm.IsNavigatingAway) return;
                 vm.ShutdownApplicationCommand.Execute(this);
                 e.Cancel = true;
+            }
+        }
+
+        private void LobbyGame_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ltBxChat.ItemsSource is INotifyCollectionChanged collection)
+            {
+                collection.CollectionChanged += (s, args) =>
+                {
+                    if (args.Action == NotifyCollectionChangedAction.Add)
+                    {
+                        if (ltBxChat.Items.Count > 0)
+                        {
+                            var ultimoMensaje = ltBxChat.Items[ltBxChat.Items.Count - 1];
+                            ltBxChat.ScrollIntoView(ultimoMensaje);
+                        }
+                    }
+                };
             }
         }
     }
